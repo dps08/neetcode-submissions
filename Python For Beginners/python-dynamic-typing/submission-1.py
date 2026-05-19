989613cabd9e0dd2052d2ea3ec3b21d23660aa24
Divit <singh.divi@northeastern.edu>
2026-05-19 14:35:33 -0400

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.3
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "String"
+print(type(variable))
+variable = [2, 3, 4]
+print(type(variable))
